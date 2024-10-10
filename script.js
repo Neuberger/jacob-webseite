48980be154d6b75fec3906b2ecb1d949a0e83fce
Neuberger <45675560+Neuberger@users.noreply.github.com>
2024-10-10 10:40:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-function toggleMenu() {
-    const menu = document.getElementById('side-menu');
-    const mainContent = document.getElementById('main-content');
+const menuBtn = document.getElementById('menu-btn');
+const navbar = document.getElementById('navbar');
+const body = document.body;
 
-    // Menü ein-/ausblenden
-    if (menu.style.width === '250px') {
-        menu.style.width = '0'; // Menü schließen
-        mainContent.style.marginLeft = '0'; // Hauptinhalt zurückschieben
-    } else {
-        menu.style.width = '250px'; // Menü öffnen
-        mainContent.style.marginLeft = '250px'; // Hauptinhalt nach rechts schieben
-    }
-}
+menuBtn.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+    container.classList.toggle('active');
+});
